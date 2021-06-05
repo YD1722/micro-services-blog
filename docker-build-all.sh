57673327c74f38e2c4201c0d1c15ee1eff777dba
yd1722 <y.dominatz@gmail.com>
2021-06-05 22:01:58 +0530

    INT | Micro Services - Taste | Add automation docker and K8

---
+++ docker-build-all.sh
@@ -0,0 +1,22 @@
+eval $(minikube -p minikube docker-env)
+
+cd posts
+docker build -t blog/post .
+
+cd ../query
+docker build -t blog/query .
+
+cd ../event-bus
+docker build -t blog/event-bus
+
+cd ../comments
+docker build -t blog/comment .
+
+cd ../comment-moderation
+docker build -t blog/comment-mod .
+
+cd ../
+
+docker images
+
+
---
+++ k8-depl-all.sh
@@ -0,0 +1,15 @@
+cd config/k8
+
+kubectl apply -f posts-depl.yaml
+kubectl apply -f comments-depl.yaml
+kubectl apply -f comment-mod-depl.yaml
+kubectl apply -f query-depl.yaml
+kubectl apply -f event-bus-depl.yaml
+
+kubectl rollout restart deployments posts-depl
+kubectl rollout restart deployments comments-depl
+kubectl rollout restart deployments comment-mod-depl
+kubectl rollout restart deployments query-depl
+kubectl rollout restart deployments event-bus-depl
+
+
